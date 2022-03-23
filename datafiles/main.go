2b7f9eee90558c4f400d4834423ab4e980d8f3b6
sukus21 <52664542+sukus21@users.noreply.github.com>
2022-03-23 14:35:55 +0100

    Prototype of the GO part

--- datafiles/main.go
+++ datafiles/main.go
@@ -1,33 +1,86 @@
+// Copyright 2017 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build example
+// +build example
+
 package main
 
 import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/hajimehoshi/go-mp3"
 )
 
+func convert(fname string) error {
+	f, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		return err
+	}
+
+	//Open output file
+	o, err := os.Create("out.raw")
+	if err != nil {
+		return err
+	}
+
+	//Write main file
+	if _, err := io.Copy(o, d); err != nil {
+		return err
+	}
+	o.Close()
+
+	//Write file with samplerate
+	o, err = os.Create("smplr")
+	if err != nil {
+		return err
+	}
+
+	//Write thing
+	binary.Write(o, binary.LittleEndian, d.SampleRate())
+	o.Close()
+
+	//Return nil, because no errors happened :)
+	return nil
+}
+
 func main() {
 
-	//Open test file
-	rawfile, err := os.Open("test.mp3")
+	//Get file name
+	if len(os.Args) != 2 {
+		close(errors.New(fmt.Sprintf("Expected 2 arguments, got %d.", len(os.Args))))
+	}
+
+	err := convert(os.Args[1])
 	if err != nil {
-		panic(err.Error())
+		close(err)
 	}
-	defer rawfile.Close()
+}
 
-	//Create encoder
-	dec, err := mp3.NewDecoder(rawfile)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	//Set up an output buffer
-	out := make([]byte, dec.Length())
-
-	//Repeat until I get bored
-	for _, err := dec.Read(out); err == nil; {
-
-	}
-
-	os.WriteFile("out.raw", out, 0666)
+func close(err error) {
+	f, _ := os.Create("error")
+	f.WriteString(err.Error())
+	f.Close()
+	os.Exit(1)
 }
